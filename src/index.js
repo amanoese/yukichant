f21e79e18c75a9a9b9374d74e340faf9639e58f2
jiro4989 <jiroron666@gmail.com>
2019-08-12 19:55:52 +0900

    :bulb: コメントだけ追加

--- src/index.js
+++ src/index.js
@@ -6,25 +6,37 @@ const dousi = require('../data/dousi.json')
 
 
 let default_encoder = (uint8text,{ meisi, dousi }) => {
+  // 2桁の16進数コードへ変換
+  // 名詞、動詞データのキーは2桁の16進数のコードである
   let textCode = [ ...uint8text ].map(v=>`0${(+v).toString(16).toUpperCase()}`.slice(-2))
 
+  // 先頭から最後の文字の1つ手前までを先頭として文字列を生成
+  // 生成する文字列は 名詞 名詞 名詞 動詞 + 。 といった規則性になる
   let heads = textCode.slice(0,-1).map((code,i)=> (i + 1) % 4 ? meisi[code] : dousi[code] + '。' )
+
+  // 最後の文字列を必ず動詞で終えることで呪文詠唱となる
   let last  = textCode.slice(-1).map(code=> dousi[code] + '。' )
+
   return [ ...heads , last ].join('')
 }
 
 let default_decoder = (encodeText,{ meisi, dousi }) => {
+  // 元の名詞、動詞のハッシュマップのキーとバリューを逆にすることでデコード用のハッシュマップとする
   let decodeHash = Object.fromEntries([
     ...Object.entries(meisi).map(([k,v])=>[v,k]),
     ...Object.entries(dousi).map(([k,v])=>[v,k])
   ])
 
+  // デコード用の正規表現に変換
+  // ex: /さざ波|その者|ほうき星よ/g
   let decodeRegExp = new RegExp(Object.keys(decodeHash).join('|'),'g')
 
+  // 正規表現にマッチするもののみに絞り込み
   let textCodeList = encodeText
     .replace('。','')
     .match(decodeRegExp) || []
 
+  // デコード用のハッシュマップからエンコード前の2桁16進数のコードを復元
   let textCode = textCodeList
     .map(v=>decodeHash[v])
     .map(v=>parseInt(v,16))
