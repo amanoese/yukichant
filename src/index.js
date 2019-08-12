a62d98cfad895a6c5358a335fe787c758c4eeff0
jiro4989 <jiroron666@gmail.com>
2019-08-12 20:14:17 +0900

    :sparkles: エンコードに使う文字列を複数から選べるようにした

--- src/index.js
+++ src/index.js
@@ -6,37 +6,43 @@ const dousi = require('../data/dousi.json')
 
 
 let default_encoder = (uint8text,{ meisi, dousi }) => {
-  // 2桁の16進数コードへ変換
-  // 名詞、動詞データのキーは2桁の16進数のコードである
+  // 2桁の16進数コードへ変換。
+  // 名詞、動詞データのキーは2桁の16進数のコードである。
   let textCode = [ ...uint8text ].map(v=>`0${(+v).toString(16).toUpperCase()}`.slice(-2))
 
-  // 先頭から最後の文字の1つ手前までを先頭として文字列を生成
-  // 生成する文字列は 名詞 名詞 名詞 動詞 + 。 といった規則性になる
-  let heads = textCode.slice(0,-1).map((code,i)=> (i + 1) % 4 ? meisi[code] : dousi[code] + '。' )
+  // 先頭から最後の文字の1つ手前までを先頭として文字列を生成。
+  // 生成する文字列は 名詞 名詞 名詞 動詞 + 。 といった規則性になる。
+  let heads = textCode
+    .slice(0,-1)
+    .map((code,i)=> (i + 1) % 4 ? meisi[code] : dousi[code])
+    .map(v=> v[Math.floor(Math.random() * v.length)])
 
-  // 最後の文字列を必ず動詞で終えることで呪文詠唱となる
-  let last  = textCode.slice(-1).map(code=> dousi[code] + '。' )
+  // 最後の文字列を必ず動詞で終えることで呪文詠唱となる。
+  let last = textCode
+    .slice(-1)
+    .map(code=> dousi[code])
+    .map(v=> v[Math.floor(Math.random() * v.length)])
 
   return [ ...heads , last ].join('')
 }
 
 let default_decoder = (encodeText,{ meisi, dousi }) => {
-  // 元の名詞、動詞のハッシュマップのキーとバリューを逆にすることでデコード用のハッシュマップとする
+  // 元の名詞、動詞のハッシュマップのキーとバリューを逆にすることでデコード用のハッシュマップとする。
+  // エンコードに使用しているハッシュマップの値はリストのため、後続の正規表現化のために先に結合する。
   let decodeHash = Object.fromEntries([
-    ...Object.entries(meisi).map(([k,v])=>[v,k]),
-    ...Object.entries(dousi).map(([k,v])=>[v,k])
+    ...Object.entries(meisi).map(([k,v])=>[v.join('|'),k]),
+    ...Object.entries(dousi).map(([k,v])=>[v.join('|'),k])
   ])
 
-  // デコード用の正規表現に変換
+  // デコード用の正規表現に変換。
   // ex: /さざ波|その者|ほうき星よ/g
   let decodeRegExp = new RegExp(Object.keys(decodeHash).join('|'),'g')
 
-  // 正規表現にマッチするもののみに絞り込み
+  // 正規表現にマッチするもののみに絞り込み。
   let textCodeList = encodeText
-    .replace('。','')
     .match(decodeRegExp) || []
 
-  // デコード用のハッシュマップからエンコード前の2桁16進数のコードを復元
+  // デコード用のハッシュマップからエンコード前の2桁16進数のコードを復元。
   let textCode = textCodeList
     .map(v=>decodeHash[v])
     .map(v=>parseInt(v,16))
